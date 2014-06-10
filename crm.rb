6ef9fb321e968aa882ff664a2ea8a46d4bf162d7
gnazarkin <gleb.nazarkin@gmail.com>
2014-06-10 12:58:21 -0400

    Edited the post method to take in params and redirect afterwards

--- crm.rb
+++ crm.rb
@@ -4,15 +4,20 @@ require_relative 'rolodex'
 
 @@rolodex = rolodex.new
 
+get '/' do
+	@crm_app_name = "My CRM"
+	erb :index
+end 
 
 # View all contacts
 get '/contacts' do
-	@contacts = []
-  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
-  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
-
 	erb :contacts
+end 
+
+post '/contacts' do 
+	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+	@@rolodex.add_contact(new_contact)
+	redirect to('/contacts')
 end 
 
 # Add a new contact
@@ -20,9 +25,7 @@ get '/contacts/new' do
 	erb :new_contact
 end 
 
-post '/contacts' do 
-	puts params
-end 
+
 
 # View a contact
 get '/contacts/:id' do

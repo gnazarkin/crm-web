a3996c39e8f85542e85ba4853394b99d1816f6cb
gnazarkin <gleb.nazarkin@gmail.com>
2014-06-13 11:06:35 -0400

    Cleaned up bugs

--- crm.rb
+++ crm.rb
@@ -3,8 +3,6 @@ require 'data_mapper'
 
 DataMapper.setup(:default, "sqlite3:database.sqlite3")
 
-@crm_app_name = "My CRM"
-
 class Contact
   include DataMapper::Resource
 
@@ -20,15 +18,11 @@ DataMapper.auto_upgrade!
 
 
 get '/' do
-	@crm_app_name = "My CRM"
-
 	erb :index
 end 
 
 # View all contacts
 get '/contacts' do
-	@crm_app_name = "My CRM"
-
   @contacts = Contact.all
 	erb :contacts
 end 
@@ -44,21 +38,15 @@ post '/contacts' do
 end 
 # Add a new contact
 get '/contact/new' do
-	@crm_app_name = "My CRM"
-
 	erb :new_contact
 end 
 
 get '/contacts/:id' do
-  @crm_app_name = "My CRM"
-
 	@contact = Contact.get(params[:id].to_i)
 	erb :show_contact
 end
 
 get '/edit_list' do
-	@crm_app_name = "My CRM"
-
   @contacts = Contact.all
 		erb :edit_list
 	end
@@ -81,8 +69,6 @@ put '/contacts/:id' do
 end
 
 get '/contacts/:id/edit' do
-	@crm_app_name = "My CRM"
-
 	@contact = Contact.get(params[:id].to_i)
   if @contact
     erb :edit_contact
@@ -102,24 +88,20 @@ delete "/contacts/:id" do
   end
 end
 
-get "/search" do
-  @crm_app_name = "My CRM"
-  erb :search
+get "/search" do  erb :search
 end
 
 post "/search" do
   puts params
-  @contact = Contact.first(:last_name => params[:last_name])
-  if @contact
-    redirect to("/contacts/#{@contact.id}")
+  @contacts = Contact.all(:last_name => params[:last_name])
+  if @contacts
+    erb :contacts
   else
     redirect to("/no_contact")
   end 
 end
 
 get "/no_contact" do
-  @crm_app_name = "My CRM"
-  
   erb :no_contact
 end
 
